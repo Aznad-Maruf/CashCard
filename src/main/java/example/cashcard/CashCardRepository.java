bf64af1faa5c6d3aeee4cbe794ff8341cd4f6591
Aznad-Maruf <aznadmaruf@gmail.com>
2024-04-14 21:52:14 +0600

    Added h2 db

---
+++ src/main/java/example/cashcard/CashCardRepository.java
@@ -0,0 +1,10 @@
+package example.cashcard;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Author: Khandaker Maruf
+ * Date: 05 Apr 2024
+ */
+public interface CashCardRepository extends CrudRepository<CashCard, Long> {
+}
